0361d0b73e00635efcd0d9a2682e4d8209219d27
Johnny Johnson <johnny2136@gmail.com>
2016-07-01 16:18:16 -0400

    Updated Sonar findings

--- src/org/johnsonland/fibonacci/big/BigFibonacci.java
+++ src/org/johnsonland/fibonacci/big/BigFibonacci.java
@@ -3,26 +3,35 @@ package org.johnsonland.fibonacci.big;
 import java.math.BigInteger;
 import java.util.Scanner;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BigFibonacci.
+ */
 public class BigFibonacci {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello fib! Enter the proposed length of Fibonacci series:");
 		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 	
 		// get user input for a
-		int a = reader.nextInt();
-		 BigInteger b = new BigInteger("0");
-	     BigInteger c = new BigInteger("1");
-		 BigInteger d = new BigInteger("0");
-		System.out.println(b);
-		System.out.println(c);
-		while (a > 0){
-			d = b.add(c);
-			b = c;
-			c = d;
-			System.out.println(d);
-			a--;
+		int i = reader.nextInt();
+		 BigInteger num1 = new BigInteger("0");
+	     BigInteger num2 = new BigInteger("1");
+		 BigInteger num3 ;
+		System.out.println(num1);
+		System.out.println(num2);
+		while (i > 0){
+			num3 = num1.add(num2);
+			num1 = num2;
+			num2 = num3;
+			System.out.println(num3);
+			i--;
 		}
 		
 	}
