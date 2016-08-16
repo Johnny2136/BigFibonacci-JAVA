f818ae3bc3e6ba9a9db41cf9843bd965a5f7edbf
Johnny Johnson <johnny2136@gmail.com>
2016-08-16 13:35:10 -0400

    induced error to test jenkins
    commented out line 24  //BigInteger num1 = new BigInteger("0");

--- src/org/johnsonland/fibonacci/big/BigFibonacci.java
+++ src/org/johnsonland/fibonacci/big/BigFibonacci.java
@@ -21,7 +21,7 @@ public class BigFibonacci {
 	
 		// get user input for a
 		int i = reader.nextInt();
-		 BigInteger num1 = new BigInteger("0");
+		 //BigInteger num1 = new BigInteger("0");
 	         BigInteger num2 = new BigInteger("1");
 		 BigInteger num3 ;
 		System.out.println(num1);
